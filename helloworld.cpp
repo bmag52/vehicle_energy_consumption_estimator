0d3177850801d9d3152cc870fccb4b5faf210ee9
HUONG NGOC QUYNH VO <huongnvo@uw.edu>
2015-11-11 15:55:05 -0800

    added template for simple cpp class in helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,27 @@
  *      Author: huongnvo
  */
 
+#include <iostream>
+using namespace std;
 
+class HelloWorld {
+	int width, height;
 
+	void set_values(int, int);
 
+	int area() {
+		return width * height;
+	}
+};
+
+void HelloWorld::set_values(int x, int y) {
+	width = x;
+	height = y;
+}
+
+int main() {
+	HelloWorld hello;
+	hello.set_values(12, 13);
+	cout << "area: " << hello.area();
+	return 0;
+}

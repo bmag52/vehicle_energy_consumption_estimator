cc279a2a0101e4cedbfcff3ffd61ec7fe9e31fb9
Brian <bmag52@uw.edu>
2016-02-16 13:44:18 -0800

    spiffing up the eclipse project file

---
+++ PTC_eclipse/src/city_amanda/Intersection.cpp
@@ -0,0 +1,28 @@
+/*
+ * Intersection.cpp
+ *
+ *  Created on: Jan 8, 2016
+ *      Author: Amanda
+ */
+
+#include "Intersection.h"
+
+namespace City {
+
+
+Intersection::Intersection(Road* roadInput, std::string intersectType,
+		std::pair<int, int>* coords, int elev, int intersectNum) {
+	roads = roadInput;
+	interSectionType = IntersectionTypes(intersectType);
+	//if length coords is 2 set equal to, obj coordinates= coords else then it is equal to 0,0
+	coordinates = coords;
+	elevation = elev;
+	number = intersectNum;
+}
+
+Intersection::~Intersection() {
+
+
+}
+
+} /* namespace City */
---
+++ PTC_eclipse/src/city_amanda/Intersection.h
@@ -0,0 +1,26 @@
+/*
+ * Intersection.h
+ *
+ *  Created on: Jan 8, 2016
+ *      Author: Amanda
+ */
+
+#ifndef CITY_INTERSECTION_H_
+#define CITY_INTERSECTION_H_
+
+namespace City {
+
+class Intersection {
+public:
+	Road* roads; //%array of roads that connect to this intersection
+	IntersectionTypes interSectionType;
+	std::pair<int, int>* coordinates;
+	int elevation;
+	int number;
+	Intersection(Road*, std::string intersectType, std::pair<int, int>*, int, int);
+	virtual ~Intersection();
+};
+
+} /* namespace City */
+
+#endif /* CITY_INTERSECTION_H_ */

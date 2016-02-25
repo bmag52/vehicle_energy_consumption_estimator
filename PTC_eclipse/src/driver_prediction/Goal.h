77f50fbd57968d87be3998d69ce8156802fd433d
Amarpal Singh <amarpal@D-69-91-136-251.dhcp4.washington.edu>
2016-02-25 11:48:15 -0800

    Adding Goal

---
+++ PTC_eclipse/src/driver_prediction/Goal.h
@@ -0,0 +1,28 @@
+/*
+ * Goal.h
+ *
+ *  Created on: Feb 23, 2016
+ *      Author: vagrant
+ */
+
+#ifndef DRIVER_PREDICTION_GOAL_H_
+#define DRIVER_PREDICTION_GOAL_H_
+
+namespace InnovationModel {
+
+class Goal {
+private:
+	int destination;
+	int bins[];
+	int size;
+public:
+	Goal(int destination, int bin[], int size);
+	Goal(Goal& other);
+	bool issimilar(Goal &other);
+	bool isequal(Goal &other);
+	virtual ~Goal();
+};
+
+} /* namespace InnovationModel */
+
+#endif /* DRIVER_PREDICTION_GOAL_H_ */

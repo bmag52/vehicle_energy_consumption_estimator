50f8fef04c4558ffd39ab2fc44ce92b01777a414
Brian <bmag52@uw.edu>
2016-03-01 11:07:44 -0800

    buildable ver. 2

--- PTC_eclipse/src/driver_prediction/Goal.cpp
+++ PTC_eclipse/src/driver_prediction/Goal.cpp
@@ -9,40 +9,40 @@
 
 namespace InnovationModel {
 
-Goal::Goal(int destination, int bin[], int size) {
-	this->destination = destination;
-	this->bins = bin;
-	this->size = size;
-}
+//Goal::Goal(int destination, int bin[], int size) {
+//	this->destination = destination;
+//	this->bins = bin;
+//	this->size = size;
+//}
 
-Goal::Goal(Goal& other) {
-	this->destination = other.destination;
-	this->size = other.size;
-	this->bins = new int[size];
-	for (size_t i = 0; i < size; i++) {
-		this->bins[i] = other.bins[i];
-	}
-}
+//Goal::Goal(Goal& other) {
+//	this->destination = other.destination;
+//	this->size = other.size;
+//	this->bins = new int[size];
+//	for (size_t i = 0; i < size; i++) {
+//		this->bins[i] = other.bins[i];
+//	}
+//}
 
-bool Goal::issimilar(Goal& other) {
-	if (this->size == other.size) {
-		for (size_t i = 0; i < size; i++) {
-			if (this->bins[i] != other.bins[i]) {
-				return false;
-			}
-		}
-		return true;
-	} else {
-		return false;
-	}
-}
+//bool Goal::issimilar(Goal& other) {
+//	if (this->size == other.size) {
+//		for (size_t i = 0; i < size; i++) {
+//			if (this->bins[i] != other.bins[i]) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	} else {
+//		return false;
+//	}
+//}
 
-bool Goal::isequal(Goal& other) {
-	return issimilar(other) && this->destination == other.destination;
-}
+//bool Goal::isequal(Goal& other) {
+//	return issimilar(other) && this->destination == other.destination;
+//}
 
-Goal::~Goal() {
-	// TODO Auto-generated destructor stub
-}
+//Goal::~Goal() {
+//	// TODO Auto-generated destructor stub
+//}
 
 } /* namespace InnovationModel */

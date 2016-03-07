c15617c1fb3df8d6b6249e428b1d65e4fb235ada
Brian <bmag52@uw.edu>
2016-03-07 14:34:25 -0800

    finished init_params function NN

--- PTC_eclipse/src/speed_prediction/speed_prediction.cpp
+++ PTC_eclipse/src/speed_prediction/speed_prediction.cpp
@@ -15,15 +15,30 @@ speed_prediction::speed_prediction(Eigen::MatrixXd * Wts)
 
 }
 
-void speed_prediction::initzialize(double * spd_trc[])
+void speed_prediction::init_params()
 {
+
+	// init HN
+	int HN[] = {80, 65, 50, 35, 20};
+	this->HN = HN;
+
+	// init input / output dimensions
 	this->alpha = 10.0;
 	this->I = 100;
-	this->HN = {80, 65, 50, 35, 20};
 	this->HL = sizeof(this->HN);
 	this->O = 14;
-//	this->totalLayers =
+
+	// init total layers
+	int totalLayers[this->HL+2] = { 0 };
+	this->totalLayers = totalLayers;
+	this->totalLayers[0] = I;
+	for(int i = 1; i < HL; i++)
+	{
+		this->totalLayers[i] = this->HN[i-1];
+	}
+	this->totalLayers[this->HL+1] = O;
 }
+
 
 }
 

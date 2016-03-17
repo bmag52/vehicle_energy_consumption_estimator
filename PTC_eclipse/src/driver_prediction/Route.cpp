babb44b9d0960b6834d1c728268cd40ec01e8640
Aishwarya Nirmal <Aishwarya@D-128-208-56-183.dhcp4.washington.edu>
2016-03-17 12:33:19 -0700

    fixed most of route

--- PTC_eclipse/src/driver_prediction/Route.cpp
+++ PTC_eclipse/src/driver_prediction/Route.cpp
@@ -5,32 +5,34 @@
  *      Author: vagrant
  */
 
+
+#include "../route_prediction/Link.h"
+#include "Goal.h"
 #include "Route.h"
 
 namespace DriverPrediction {
-using namespace InnovationModel;
 
-Route::Route(Link links[], Goal &goal) {
-	this->links = links;
+Route::Route(Link links[], Goal& goal) {
+	(*this->links) = *links;
 	this->goal = goal;
 	this->counter = 0;
 }
 
 // adds new link to end of route
-void Route::addlink(Link &link) {
-	int length = sizeof(this->links) / sizeof(Link);
-	Link temp[] = new Link[length];
+void Route::addlink(Link link) {
+	int length = (sizeof(this->links))/(sizeof(this->links[0]));
+	Link temp[length + 1];
 	for(int i = 0; i < length; i++) {
 		temp[i] = links[i];
 	}
 	temp[length] = link;
-	this->links = temp;
+	(*this->links) = (*temp);
 }
 
 // checks if the route is equal to the route passed in
 bool Route::isequal(Route &other) {
-	int mylength = sizeof(this->links) / sizeof(Link);
-	int otherlength = sizeof(other.links) / sizeof(Link);
+	int mylength = sizeof(this->links) / sizeof(this->links[0]);
+	int otherlength = sizeof(other.links) / sizeof(other.links[0]);
 	if(!(mylength == otherlength) || !((this->goal).isequal(other.goal))) {
 		return false;
 	}
@@ -44,22 +46,22 @@ bool Route::isequal(Route &other) {
 
 //returns copy of current route
 Route Route::copy() {
-	goal = Goal(this->goal.destination, this->goal.bins, this->goal.size);	//is this the right? Goal variables are all private, but not sure if there's a better way
-	int length = sizeof(this->links) / sizeof(Link);
-	links = Link[length];
+	Goal newGoal = Goal(this->goal.getDestination(), &(this->goal.getBins()), this->goal.getSize());	//is this the right? Goal variables are all private, but not sure if there's a better way
+	int length = sizeof(this->links) / sizeof(this->links[0]);
+	Link newLinks[length];
 	for(int i = 0; i < length; i++) {
 		int number = this->links[i].getNumber();
 		int direction = this->links[i].getDirection();
-		links[i] = this->links[i].copy(direction, number);
+		newLinks[i] = this->links[i].copy(direction, number);
 	}
-	return Route(links, goal);
+	return Route(newLinks, newGoal);
 }
 
 //returns next link in the iterator, returns 0 if there are no more links left
 Link Route::nextlink() {
 	int length = sizeof(this->links) / sizeof(Link);
-	if(this->counter > length || this->counter < 1) {
-		return 0;
+	if(this->counter == length || this->counter < 1) {
+		return this->links[this->counter].final_link(); // not sure if this is ok
 	} else {
 		Link link = this->links[this->counter];
 		this->counter++;
--- PTC_eclipse/src/driver_prediction/Route.h
+++ PTC_eclipse/src/driver_prediction/Route.h
@@ -4,9 +4,11 @@
  *  Created on: Mar 6, 2016
  *      Author: vagrant
  */
+#include "../route_prediction/Link.h"
+#include "Goal.h"
+
 
 namespace DriverPrediction {
-using namespace InnovationModel;
 
 class Route {
 private:
@@ -14,9 +16,9 @@ private:
 	Goal goal;
 	int counter;
 public:
-	Route(Link links[], Goal &goal);
-	void addlink(Link &link);
-	bool isequal(Route &other);
+	Route(Link links[], Goal& goal);
+	void addlink(Link link);
+	bool isequal(Route& other);
 	Route copy();
 	Link nextlink();
 };

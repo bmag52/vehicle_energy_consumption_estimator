57beef7cffbe6c9727307ce8b77a517092e8c89c
Aishwarya Nirmal <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2016-03-26 15:07:44 -0700

    a few changes to Route

--- PTC_eclipse/src/route_prediction/Route.cpp
+++ PTC_eclipse/src/route_prediction/Route.cpp
@@ -17,43 +17,53 @@ Route::Route() {
 	this->counter = 0;
 }
 
+Route::Route(Link links[], Goal& goal) {
+	this->counter = 0;
+	this->links = links;
+	this->goal = goal;
+}
+
 // adds new link to end of route
 void Route::addlink(Link link) {
-//	int length = sizeof(this->links) / sizeof(Link);
-//	Link temp[] = new Link[length];
-//	for(int i = 0; i < length; i++) {
-//		temp[i] = links[i];
-//	}
-//	temp[length] = link;
-//	this->links = temp;
+	int length = sizeof(this->links) / sizeof(Link);
+	Link temp[] = new Link[length];
+	for(int i = 0; i < length; i++) {
+		temp[i] = links[i];
+	}
+	temp[length] = link;
+	this->links = temp;
 }
 
 // checks if the route is equal to the route passed in
-bool Route::isequal(Route other) {
-//	int mylength = sizeof(this->links) / sizeof(Link);
-//	int otherlength = sizeof(other.links) / sizeof(Link);
-//	if(!(mylength == otherlength) || !((this->goal).isequal(other.goal))) {
-//		return false;
-//	}
-//	for(int i = 0; i < mylength; i++) {
-//		if(!(this->links[i].isEqual(other.links[i]))) {
-//			return false;
-//		}
-//	}
-//	return true;
+bool Route::isequal(Route& other) {
+	int mylength = sizeof(this->links) / sizeof(Link);
+	int otherlength = sizeof(other.links) / sizeof(Link);
+	if(!(mylength == otherlength) || !((this->goal).isequal(other.goal))) {
+		return false;
+	}
+	for(int i = 0; i < mylength; i++) {
+		if(!(this->links[i].isEqual(other.links[i]))) {
+			return false;
+		}
+	}
+	return true;
 }
 
 //returns copy of current route
 Route Route::copy() {
-//	Goal goal = Goal(this->goal.destination, this->goal.bins, this->goal.size);	//is this the right? Goal variables are all private, but not sure if there's a better way
-//	int length = sizeof(this->links) / sizeof(Link);
-//	links = Link[length];
-//	for(int i = 0; i < length; i++) {
-//		int number = this->links[i].getNumber();
-//		int direction = this->links[i].getDirection();
-//		links[i] = this->links[i].copy(direction, number);
-//	}
-//	return Route(links, goal);
+	Goal goal = Goal(this->goal.destination, this->goal.bins, this->goal.size);
+	int length = sizeof(this->links) / sizeof(Link);
+	links = Link[length];
+	for(int i = 0; i < length; i++) {
+		int number = this->links[i].getNumber();
+		int direction = this->links[i].getDirection();
+		links[i] = this->links[i].copy(direction, number);
+	}
+	return Route(links, goal);
+}
+
+void initiateiterator() {
+	this->counter = 0;
 }
 
 //returns next link in the iterator, returns 0 if there are no more links left
--- PTC_eclipse/src/route_prediction/Route.h
+++ PTC_eclipse/src/route_prediction/Route.h
@@ -8,18 +8,16 @@
 #include "../route_prediction/Goal.h"
 
 
-<<<<<<< HEAD:PTC_eclipse/src/driver_prediction/Route.h
 namespace DriverPrediction {
 
 using namespace InnovationModel;
-=======
+
 #include "Goal.h"
 #include "../driver_prediction/Link.h"
 
 using namespace DriverPrediction;
 
 namespace RoutePrediction {
->>>>>>> master:PTC_eclipse/src/route_prediction/Route.h
 
 class Route {
 private:
@@ -27,16 +25,12 @@ private:
 	Goal goal;
 	int counter;
 public:
-<<<<<<< HEAD:PTC_eclipse/src/driver_prediction/Route.h
+	Route();
 	Route(Link links[], Goal& goal);
 	void addlink(Link link);
 	bool isequal(Route& other);
-=======
-	Route();
-	void addlink(Link link);
-	bool isequal(Route other);
->>>>>>> master:PTC_eclipse/src/route_prediction/Route.h
 	Route copy();
+	void initiateiterator();
 	Link nextlink();
 };
 

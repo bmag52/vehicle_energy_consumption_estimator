d6fb99594ef88cc3ab685fd21903f75d162b558e
Archana Simha <Archana@D-69-91-134-106.dhcp4.washington.edu>
2016-04-05 15:58:49 -0700

    Added probability unit tests and template for intersection unit test

---
+++ PTC_eclipse/src/unit_tests/IntersectionUnitTest.cpp
@@ -0,0 +1,20 @@
+/*
+ * IntersectionUnitTest.cpp
+ *
+ *  Created on: Apr 5, 2016
+ *      Author: vagrant
+ */
+#include "../city/Intersection.h"
+#include <iostream>
+#include <assert.h>
+#include "UnitTests.h"
+
+using namespace std;
+
+
+void intersection_ut() {
+
+}
+
+
+

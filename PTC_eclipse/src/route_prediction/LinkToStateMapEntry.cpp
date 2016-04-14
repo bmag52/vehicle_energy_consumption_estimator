448ed5110c3a077dadda20e767dd890fef3ac4ab
Silin <silin@flexminder.com>
2016-04-13 21:15:14 -0700

    finishing LinkToStateMapEntry class and unit test, adding updateEntry() method to GenericMap

--- PTC_eclipse/src/route_prediction/LinkToStateMapEntry.cpp
+++ PTC_eclipse/src/route_prediction/LinkToStateMapEntry.cpp
@@ -6,28 +6,57 @@
  */
 
 #include "LinkToStateMapEntry.h"
+#include <iostream>
 
+using namespace std;
 namespace PredictivePowertrain {
 
 LinkToStateMapEntry::LinkToStateMapEntry() {
 }
 
+//adds the link to the object, if this is the first time this link
+//is added to m, m=1, otherwise, m++ for that link
 int LinkToStateMapEntry::addEntry(Link* li) {
+	int linkHash = li->getHash();
+	if (this->entries.hashInMap(linkHash)){
+		int existingM = this->entries.getEntry(linkHash);
+		this->entries.updateEntry(linkHash, existingM + 1);
+	} else {
+		this->entries.addEntry(linkHash, 1);
+	}
 }
 
 LinkToStateMapEntry::~LinkToStateMapEntry() {
 }
 
 LinkToStateMapEntry::LinkToStateMapEntry(LinkToStateMapEntry& other) {
+	this->entries = other.entries;
 }
 
 int LinkToStateMapEntry::getM(Link* li) {
+	int linkHash = li->getHash();
+	if (this->entries.hashInMap(linkHash)){
+		return this->entries.getEntry(linkHash);
+	} else {
+		return 0;
+	}
 }
 
 int LinkToStateMapEntry::getTotalM() {
+	int sum = 0;
+	if (this->entries.getSize() == 0){
+		return 0;
+
+	} else {
+		for (auto it = this->entries.begin(); it != this->entries.end(); ++it){
+			sum += it->second;
+		}
+		return sum;
+	}
 }
 
 int LinkToStateMapEntry::totalM(LinkToStateMapEntry* linkToStateMapEntry) {
+		return linkToStateMapEntry->getTotalM();
 }
 
 } /* namespace PredictivePowertrain */

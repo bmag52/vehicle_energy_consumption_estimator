9ccac7693c951f396788d608eedf448801ad1a2d
Silin <silin@flexminder.com>
2016-04-19 16:16:50 -0700

    the correct node unit test commit

--- PTC_eclipse/src/unit_tests/NodeUnitTest.cpp
+++ PTC_eclipse/src/unit_tests/NodeUnitTest.cpp
@@ -1,11 +1,12 @@
 /*
  * NodeUnitTest.cpp
  *
- *  Created on: Apr 14, 2016
+ *  Created on: Apr 19, 2016
  *      Author: vagrant
  */
 
-#include "../data_collection/Node.h"
+
+#include "../data_management/Node.h"
 #include <iostream>
 #include <assert.h>
 #include "UnitTests.h"
@@ -15,25 +16,24 @@ using namespace PredictivePowertrain;
 
 void node_ut(){
 
-Node node1(0,0,0);
-Node node2(1.0, 1.0, 1);
-Node node3(15.5, 16.5, 20);
+	Node node1(0,0,0,0);
+	Node node2(1.0, 1.0, 1, 1);
+	Node node3(15.5, 16.5, 20, 22);
 
-assert(node1.getLat() == 0);
-assert(node1.getLon() == 0);
-assert(node1.getID() == 0);
+	assert(node1.getLat() == 0);
+	assert(node1.getLon() == 0);
+	assert(node1.getEle() == 0);
+	assert(node1.getID() == 0);
 
-assert(node2.getLat() == 1.0);
-assert(node2.getLon() == 1.0);
-assert(node2.getID() == 1);
 
-assert(node3.getLat() == 15.5);
-assert(node3.getLon() == 16.5);
-assert(node3.getID() == 20);
+	assert(node2.getLat() == 1.0);
+	assert(node2.getLon() == 1.0);
+	assert(node2.getEle() == 1);
+	assert(node2.getID() == 1);
 
-//missing: getEle(). Unsure what the expected value should be.
+	assert(node3.getLat() == 15.5);
+	assert(node3.getLon() == 16.5);
+	assert(node3.getEle() == 20);
+	assert(node3.getID() == 22);
 
 }
-
-
-
--- PTC_eclipse/src/unit_tests/UnitTests.h
+++ PTC_eclipse/src/unit_tests/UnitTests.h
@@ -12,11 +12,9 @@ void link_UT();
 void route_ut();
 void intersection_ut();
 void linkToStateMapEntry_ut();
-<<<<<<< HEAD
 void linkToStateMap_ut();
-=======
 void node_ut();
->>>>>>> d3d7bd8d8984ddeb1b1cbdb8fcf31a1e54cf357e
+
 
 
 #endif /* UNIT_TESTS_UNITTESTS_H_ */
